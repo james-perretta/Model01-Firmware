d1e6d25c0b865e468ecce2b83f807ebd65fcc0fc
James P <jameslp@umich.edu>
2018-03-29 15:00:20 -0400

    Moved brace and parenthesis location.

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -165,16 +165,16 @@ const Key keymaps[][ROWS][COLS] PROGMEM = {
 
   [FUNCTION] =  KEYMAP_STACKED
   (___,                       Key_F1,            Key_F2,      Key_F3,                   Key_F4,                   Key_F5,               Key_LEDEffectNext,
-   Consumer_VolumeIncrement,  ___, ___, Key_LeftBracket,          Key_RightBracket,         LSHIFT(Key_Backtick), Key_Delete,
-   Consumer_VolumeDecrement,  ___, ___, Key_LeftCurlyBracket,     Key_RightCurlyBracket,    Key_Slash,
-   Consumer_Mute,             ___, ___, ___,                      Key_Quote,                Key_Pipe,              LCTRL(Key_Z),
+   Consumer_VolumeIncrement,  ___, ___, Key_LeftCurlyBracket,     Key_RightCurlyBracket,    LSHIFT(Key_Backtick), Key_Delete,
+   Consumer_VolumeDecrement,  ___, ___, Key_LeftParen,            Key_RightParen,           Key_Slash,
+   Consumer_Mute,             ___, ___, Key_LeftBracket,          Key_RightBracket,         LSHIFT(Key_1),        LCTRL(Key_Z),
    ___, Key_Enter, ___, ___,
    ___,
 
    Key_CapsLock, Key_F6,                 Key_F7,                   Key_F8,                   Key_F9,              Key_F10,          Key_F11,
    ___, Key_Backtick,           Key_Minus,                Key_UpArrow,              LSHIFT(Key_Equals),  ___,              Key_F12,
         Key_Equals,             Key_LeftArrow,            Key_DownArrow,            Key_RightArrow,      ___,              ___,
-   ___, Key_Backslash,          LSHIFT(Key_Quote),        Key_LeftParen,            Key_RightParen,      Key_Backslash,    Key_Pipe,
+   ___, Key_Backslash,          LSHIFT(Key_Quote),        Key_Quote,                Key_Pipe,            ___,     ___,
    ___, ___, Key_Tab, ___,
    ___)
 
